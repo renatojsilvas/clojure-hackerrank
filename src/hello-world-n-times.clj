5d94ce67fef48df6c481bd7b83fbb739d49cd9a9
Renato José da Silva <renatojsilvas@gmail.com>
2021-08-03 12:49:30 -0300

    Hello World N times solved

--- src/hello-world-n-times.clj
+++ src/hello-world-n-times.clj
@@ -5,6 +5,7 @@
 ; https://www.hackerrank.com/challenges/fp-hello-world-n-times/problem
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+; dotimes is a function that has no return. That is perfect for side effecting functions.
 (defn hello_word_n_times [n] (dotimes [_ n] (println "Hello World")))
 
 (def n (Integer/parseInt (read-line)))

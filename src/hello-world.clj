d199e862a855ba18014c5a38abf9f00e92c1485b
Renato José da Silva <renatojsilvas@gmail.com>
2021-08-03 09:54:24 -0300

    Hello World solved

---
+++ src/hello-world.clj
@@ -0,0 +1,8 @@
+(ns hello-world)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Hacker Rank Challenges: Hello World
+; https://www.hackerrank.com/challenges/fp-hello-world/problem
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(print "Hello World")
